d1103dd5b921bbef41c3fd168e29c51c5f74f767
René Kopeinig <renekope@gmail.com>
2016-11-10 21:55:03 -0600

    001-Leaflet-BaseLayers

--- 001-Leaflet-BaseLayers/app.js
+++ 001-Leaflet-BaseLayers/app.js
@@ -7,7 +7,7 @@ var map = L.map('map', {
 });
 
 // Set the position and zoom level of the map
-map.setView([47.444872, 14.033671], 8);
+map.setView([47.444872, 14.033671], 7);
 
 /*	Variety of base layers */
 var osm_mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
